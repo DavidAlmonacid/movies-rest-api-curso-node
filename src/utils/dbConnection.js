17a6fed92bbb52156af02a4190360b4ccf3444b9
David Almonacid <david.almonacidf@gmail.com>
2024-01-12 16:31:23 -0500

    Commented out MySQL code and imports in dbConnection.js

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,15 +1,15 @@
-import mysql from 'mysql2/promise';
 import pg from 'pg';
-import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from '../config.js';
+// import mysql from 'mysql2/promise';
+// import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from '../config.js';
 
 // MySQL
-export const connection = await mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  port: DB_PORT,
-  password: DB_PASS,
-  database: DB_NAME
-});
+// export const connection = await mysql.createConnection({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   port: DB_PORT,
+//   password: DB_PASS,
+//   database: DB_NAME
+// });
 
 // PostgreSQL
 const { Pool } = pg;
